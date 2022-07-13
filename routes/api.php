2bca1a343256458594b92c62f292e9fc635fe57d
unknown <sujataekodus>
2022-07-13 12:22:35 -0700

    cart index

--- routes/api.php
+++ routes/api.php
@@ -61,7 +61,7 @@ Route::post('board-class-subject', [CourseController::class, 'findBoardClassSubj
 
 //laravel cart
 Route::group(['prefix' => 'cart','middleware' => ['auth:sanctum']], function() {
-    Route::get('',[CartController::class,'index']);
+    Route::get('/',[CartController::class,'index']);
     Route::post('/store', [CartController::class,'store']);
     Route::get('/remove/{cart_id}', [CartController::class,'remove']);
 });
