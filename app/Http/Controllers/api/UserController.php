cf8026de339b1877fa218a413da523fb7c0b6768
unknown <sujataekodus>
2022-08-18 12:20:12 -0700

    get user courses api

--- app/Http/Controllers/api/UserController.php
+++ app/Http/Controllers/api/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Cart;
 use App\Models\Order;
 use App\Models\UserDetails;
 use Illuminate\Http\Request;
@@ -89,54 +90,69 @@ class UserController extends Controller
             if ($file = $request->file('image')) {
                 $new_imgage_name = time() . '-' . auth()->user()->name . '.' . $image->extension();
                 $image_path = $image->move(public_path('files/profile'), $new_imgage_name);
-                $path_name= 'files/profile/'. $new_imgage_name;
+                $path_name = 'files/profile/' . $new_imgage_name;
                 UserDetails::where('email', auth()->user()->email)->update(['image' => $path_name]);
-                $data=[
+                $data = [
                     "code" => 200,
                     "message" => "Profile photo uploaded",
-                    "path"=>$path_name,
+                    "path" => $path_name,
                 ];
                 return response()->json(['status' => 1, 'result' => $data]);
             }
         } catch (\Throwable $th) {
-            $data=[
+            $data = [
                 "code" => 400,
                 "message" => "Something went wrong.",
-                
+
             ];
             return response()->json(['status' => 0, 'result' => $data]);
         }
     }
-    public function allCourses(Request $request){
+    public function allCourses(Request $request)
+    {
         try {
-            $orders = Order::with(['board','assignClass','assignSubject'=>function($q){
-                $q->with(['subject']);
-            }])->where('user_id',auth()->user()->id)->where('payment_status','paid')->orderBy('updated_at','DESC')->get();
-            
-            if(!$orders->isEmpty()){
-                foreach($orders as $key=>$order){
-                    $subjects=[];
-                    foreach($order->assignSubject as $key=>$assignSubject){
-                        $subject=[
-                            'name'=>$assignSubject->subject->subject_name,
-                            'image'=>$assignSubject->subject->image,
-                            'amount'=>$assignSubject->amount,
-                            'board'=>$order->board->exam_board,
-                            'class'=>$order->assignClass->class,
+            $carts = Cart::select('id', 'user_id', 'is_full_course_selected', 'assign_class_id', 'board_id', 'is_paid', 'is_remove_from_cart')
+                ->with(['assignClass:id,class', 'board:id,exam_board', 'assignSubject:id,cart_id,assign_subject_id,amount', 'assignSubject.subject:id,subject_name,image'])
+                ->where('user_id', auth()->user()->id)
+                ->where('is_paid', 1)
+                ->where('is_remove_from_cart', 0)
+                ->get();
+
+            if (!$carts->isEmpty()) {
+                foreach ($carts as $key => $cart) {
+                    $subjects = [];
+                    foreach ($cart->assignSubject as $key => $assignSubject) {
+                        $subject = [
+                            'name' => $assignSubject->subject->subject_name,
+                            'image' => $assignSubject->subject->image,
+                            'amount' => $assignSubject->amount,
+                            'board' => $cart->board->exam_board,
+                            'class' => $cart->assignClass->class,
                         ];
-                        $subjects[]=$subject;
+                        $subjects[] = $subject;
                     }
+
+                    $course_details = [
+                        'id' => $cart->id,
+                        'user_id' => $cart->user_id,
+                        'type' => $cart->is_full_course_selected,
+                        'board' => $cart->board->exam_board,
+                        'class_name' => $cart->assignClass->class,
+                        'total_subject'=>$cart->assignSubject->count(),
+                        'subject_details' => $subjects,
+
+                    ];
+                    $data = [
+                        "code" => 200,
+                        "message" => "Subject Details",
+                        "courses" => $course_details,
+
+                    ];
+                    return response()->json(['status' => 1, 'result' => $data]);
                 }
-                
-                $data = [
-                    "code" => 200,
-                    "status" => 1,
-                    "message" => "All Subjects",
-                    "all_subjects" => $subjects
-    
-                ];
-                return response()->json(['status' => 1, 'result' => $data]);
             }
+
+
            
             $data = [
                 "code" => 200,
@@ -146,6 +162,7 @@ class UserController extends Controller
 
             ];
             return response()->json(['status' => 1, 'result' => $data]);
+
         } catch (\Throwable $th) {
             $data = [
                 "code" => 400,
