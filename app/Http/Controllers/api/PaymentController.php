8588a9e3546d5077f2c768c7d5bf81bc7eb15c85
unknown <sujataekodus>
2022-08-25 11:57:13 -0700

    razorpay payment,order api

--- app/Http/Controllers/api/PaymentController.php
+++ app/Http/Controllers/api/PaymentController.php
@@ -74,7 +74,7 @@ class PaymentController extends Controller
         try {
             $cart_id = $request->cart_id;
             $razorpay_order_id =  $request->razorpay_order_id;
-            $razorpay_payment_id = $request->razorpay_payment_id??null;
+            $razorpay_payment_id = $request->razorpay_payment_id;
            
             $cart = Cart::find($cart_id);
             
@@ -102,7 +102,7 @@ class PaymentController extends Controller
             $data = [
                 "code" => 400,
                 "status" => 0,
-                "message" => "Something went wrong.",
+                "message" => $th,
 
 
             ];
