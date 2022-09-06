533c9462ded59e615dccb958e9866528eea894ff
unknown <sujataekodus>
2022-09-06 07:19:58 -0700

    api for preformance

--- app/Http/Controllers/api/UserController.php
+++ app/Http/Controllers/api/UserController.php
@@ -302,7 +302,7 @@ class UserController extends Controller
             if ($istype == 2) {
                 
                 $user = User::where('email',$request->email)->where('verify_otp', 1)->first();
-                return response()->json($user);
+                
                 if ($user) {
                     $otp = rand(100000, 999999);
                     $user->update(['otp' => $otp]);
