00da3dcf52733839f9ae9c9974cc604c808d9813
unknown <sujataekodus>
2022-09-06 07:33:15 -0700

    api for preformance

--- app/Http/Controllers/api/UserController.php
+++ app/Http/Controllers/api/UserController.php
@@ -355,9 +355,10 @@ class UserController extends Controller
             if (!checkemail($request->user_id)) {
                 $user = user::where('email', $request->user_id)->where('otp', $request->otp)->first();
             } else {
+                return response()->json($request->user_id);
                 $user = user::where('phone', $request->user_id)->where('otp', $request->otp)->first();
             }
-            return response()->json($user);
+            
             if ($user) {
                 $data = [
                     "user_id" => $user->id,
