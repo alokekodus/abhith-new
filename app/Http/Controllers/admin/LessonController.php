d6d42135f70fce053b2c6e97660fa6d4d516de52
unknown <sujataekodus>
2022-09-13 14:30:27 -0700

    fixed resourse update

--- app/Http/Controllers/admin/LessonController.php
+++ app/Http/Controllers/admin/LessonController.php
@@ -202,7 +202,7 @@ class LessonController extends Controller
     public function topicStore(Request $request)
     {
         try { 
-            
+            dd($request->all());
             $isLessonNameAlreadyInUsed = Lesson::where('name', $request->name)->first();
             if ($isLessonNameAlreadyInUsed) {
                 Toastr::error('This Resource name already in used.', '', ["positionClass" => "toast-top-right"]);
