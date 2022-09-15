469d267bc5cf5e3b1b70d25913eff15fb3d80e5e
unknown <sujataekodus>
2022-09-14 17:42:39 -0700

    student subject enroll

--- app/Http/Controllers/teacher/StudentController.php
+++ app/Http/Controllers/teacher/StudentController.php
@@ -46,7 +46,7 @@ class StudentController extends Controller
   public function getAssignOrder($subject_id)
   {
     $assign_subjects = AssignSubject::with(['assignOrder' => function ($query) {
-      $query->with(['order' => function ($q) {
+      $query->with(['assignOrder' => function ($q) {
         $q->with('user');
       }]);
     }])->where('id', Crypt::decrypt($subject_id))->first();
