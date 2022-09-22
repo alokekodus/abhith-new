2fc2d9c4f90cb86986805812e1580f0091b4799e
unknown <sujataekodus>
2022-09-22 11:26:06 -0700

    all updated

--- database/seeders/AdminDetailsSeeder.php
+++ database/seeders/AdminDetailsSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use App\Models\UserDetails;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class AdminDetailsSeeder extends Seeder
 {
@@ -14,10 +16,15 @@ class AdminDetailsSeeder extends Seeder
      */
     public function run()
     {
-        $user=UserDetails::create([
+        $user=User::create([
             'name' => 'Admin',
-            'email' => 'abhit@admin.com',
+            'email' => 'admin@abhit.com',
+            'phone' => '7896541230',
+            'otp' => '123456',
+            'verify_otp' => 1,
+            'password' => Hash::make('123456') ,
             'type_id' => 1,
+            'is_activate' => 1
         ]);
         $roles = 1;
         $user->assignRole($roles);
