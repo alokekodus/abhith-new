f7a1fa26b85a485ffcba075b3904a1104d961412
unknown <sujataekodus>
2022-09-23 11:43:21 -0700

    banner limit

--- app/Http/Controllers/website/CartController.php
+++ app/Http/Controllers/website/CartController.php
@@ -53,6 +53,7 @@ class CartController extends Controller
     }
     public function addToCart(Request $request)
     {
+        
         try {
               
             if (!Auth::check()) {
@@ -69,67 +70,7 @@ class CartController extends Controller
             } else {
                 $all_subjects = $request->subjects;
             }
-           
-            $check_item_exists_inside_cart = Cart::with('assignSubject')->where('user_id', Auth::user()->id)->where('board_id', $board_id)->where('assign_class_id', $class_id)->where([['is_paid', '=', 0], ['is_remove_from_cart', '=', 0]])->where('is_full_course_selected',1)->get();
-           
-            if ($all_subjects == null) {
-                Toastr::error('Please select subject for proccess !', '', ["positionClass" => "toast-top-right"]);
-                return redirect()->back();
-            }
-            if ($check_item_exists_inside_cart->count() > 0) {
-
-                Toastr::error('Package already in Cart!', '', ["positionClass" => "toast-top-right"]);
-                return redirect()->back();
-            } else {
-
-                $cart_check = Cart::whereHas('assignSubject', function ($q) use ($all_subjects) {
-                    $q->whereIn('assign_subject_id', $all_subjects);
-                })->where('board_id', $board_id)->where('assign_class_id', $class_id)->where('is_remove_from_cart',0)->where('user_id',auth()->user()->id)->where('is_buy',$request->is_buy)->first();
-                
-                if ($cart_check) {
-                   $already_in_cart=false;
-                    foreach($all_subjects as $key=>$subject){
-                        $is_available=$cart_check->assignSubject()->where('assign_subject_id',$all_subjects[$key])->first();
-                        
-                        
-                    }
-                   
-                    if($cart_check->is_full_course_selected==$request->course_type && $cart_check->assignSubject()->count()==count($all_subjects) && $request->is_buy==0){
-                        Toastr::error('Same Subjects already on your cart.', '', ["positionClass" => "toast-top-right"]);
-                        return redirect()->back();
-                    }
-                    $cart_check->assignSubject()->delete();
-                    $cart_check->update(['is_remove_from_cart'=>1]);
-                 
-                }
-                $cart = Cart::create([
-                    'user_id' => auth()->user()->id,
-                    'board_id' => $board_id, //board_id
-                    'assign_class_id' => $class_id, //class_id
-                    'is_full_course_selected' => $course_type,
-                    'is_buy' => $request->is_buy
-                ]);
-                foreach ($all_subjects as $key => $subject) {
-
-
-                    $subject = AssignSubject::find($all_subjects[$key]);
-    
-                    $data = [
-                        'cart_id' => $cart->id,
-                        'assign_subject_id' => $subject->id,
-                        'amount' => $subject->subject_amount,
-                        'type' => 1,
-                    ];
-                    $assign_subject = CartOrOrderAssignSubject::create($data);
-                }
-
-
-                Toastr::success('Subjects was successfully added to your cart.', '', ["positionClass" => "toast-top-right"]);
-                return redirect()->back();
-
-
-            
-            }
+            dd($all_subjects);
             
         } catch (\Throwable $th) {
             
