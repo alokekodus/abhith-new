715a784fd94c7d729060ff762833d4bbac8c9e74
unknown <sujataekodus>
2022-09-23 11:52:03 -0700

    banner limit

--- app/Http/Controllers/api/BannerController.php
+++ app/Http/Controllers/api/BannerController.php
@@ -11,7 +11,7 @@ class BannerController extends Controller
     public function index()
     {
         try {
-            $banner = Banner::select('name', 'banner_image', 'description')->where('is_activate', 1)->order_by('created_at', 'desc')->get();
+            $banner = Banner::select('id','name', 'banner_image', 'description','created_at')->where('is_activate', 1)->order_by('created_at', 'desc')->take(5)->get();
             $result = ["banner" => $banner];
             if (!$banner->isEmpty()) {
                 $data = [
