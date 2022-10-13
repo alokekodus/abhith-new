40cc0c3b000802379602f3ab782a394b40620d53
unknown <sujataekodus>
2022-10-13 14:17:42 -0700

    api change

--- app/helpers.php
+++ app/helpers.php
@@ -299,11 +299,11 @@ function subjectAlreadyPurchase($subject_id)
     }
 }
 function subjectAlreadyInCart($subject_id){
-    
+
     $subject=AssignSubject::find($subject_id);
     $board_id=$subject->board_id;
     $class_id=$subject->assign_class_id;
-    $cart_check = Cart::whereHas('assignSubject', function ($q) use ($subject_id,$board_id,$class_id) {
+    $cart_check = Cart::whereHas('assignSubject', function ($q) use ($subject_id) {
         $q->whereIn('assign_subject_id', $subject_id);
     })->where('board_id', $board_id)->where('assign_class_id', $class_id)->where('is_remove_from_cart',0)->where('user_id',auth()->user()->id)->first();
     if($cart_check){
