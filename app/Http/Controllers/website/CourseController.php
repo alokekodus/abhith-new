2e4529006b07d26a20f13a319f0014999e91471e
Alok <alok.deka@ekodus.com>
2022-10-28 16:35:31 +0530

    Lesson title added on video

--- app/Http/Controllers/website/CourseController.php
+++ app/Http/Controllers/website/CourseController.php
@@ -231,7 +231,7 @@ class CourseController extends Controller
     }
 
     public function video($id){
-        $data['video'] = LessonAttachment::find(Crypt::decrypt($id));
+        $data['video'] = LessonAttachment::where('id', Crypt::decrypt($id))->with('lesson')->first();
         return view('website.my_account.video')->with($data);
     }
 }
--- resources/views/website/my_account/video.blade.php
+++ resources/views/website/my_account/video.blade.php
@@ -9,7 +9,7 @@
 
     @include('layout.website.include.forum_header')
     <div class="container">
-        <h3 class="text-center">Video title</h3>
+        <h3 class="text-center">{{$video->lesson->name}}</h3>
         <video poster="{{ asset($video->video_thumbnail_image) }}" controls style="width: 100%;">
             <source src="{{ asset($video->video_origin_url) }}" type="video/mp4">
             Your browser does not support the video tag.
