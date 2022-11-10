29c6b410a42ee02b971543cba7606ef7284ebaeb
unknown <sujataekodus>
2022-11-10 16:00:53 +0500

    video display courses

--- resources/views/website/my_account/video.blade.php
+++ resources/views/website/my_account/video.blade.php
@@ -8,9 +8,12 @@
 @section('content')
 
 @include('layout.website.include.forum_header')
-<div class="container">
-    @dump($data)
-    <h3 class="text-center"></h3>
+@section('content')
+<div class="container-fluid">
+    
+        <h3 class="text-center">{{$data->lesson->name}}</h3>
+  
+
     {{-- <video poster="{{ asset($video->video_thumbnail_image) }}" controls style="width: 100%;">
         <source src="{{ asset($video->video_origin_url) }}" type="video/mp4">
         Your browser does not support the video tag.
@@ -19,6 +22,7 @@
         poster="{{ asset($data->video_thumbnail_image) }}" loading="lazy">
     </video>
 </div>
+@endsection
 @section('scripts')
 <script src="{{asset('asset_website/js/videojs.watermark.js')}}"></script>
 <script src="{{asset('asset_website/js/videojs-resolution-switcher.js')}}"></script>
@@ -30,10 +34,7 @@
   
 </script>
 <script>
-    
-   
-
-        var lesson_attachment=@json($data);
+    var lesson_attachment=@json($data);
         var storagePath = "{!! storage_path() !!}";
         var FULLHD= lesson_attachment['origin_video_url'] ;
         var SD= lesson_attachment['video_resize_480'] ;
@@ -49,28 +50,28 @@
         });
         player.updateSrc([
         {
-            src: 'http://localhost/abhith-new/public/storage/'+SD,
+            src: 'http://localhost/abhith-new/public'+SD,
             type: 'video/mp4',
             res: 480,
             label: '480px'
         },
         {
-            src: 'http://localhost/abhith-new/public/storage/'+HD,
+            src: 'http://localhost/abhith-new/public'+HD,
             type: 'video/mp4',
             res: 720,
             label: '720px'
         },
             {
-            src: 'http://localhost/abhith-new/public/storage/'+FULLHD,
+            src: 'http://localhost/abhith-new/public'+FULLHD,
             type: 'video/mp4',
             res: 1080,
             label: '1080px'
         },
         
         ]);
-        player.watermark({
-            file: 'watermarks.png',
+        // player.watermark({
+        //     file: 'http://localhost/abhith-new/public/asset_website/img/home/logo_.png',
            
-        });
+        // });
 </script>
 @endsection
