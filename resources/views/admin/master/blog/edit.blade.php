d94481ec9dc7d5c19e5d99279ee62bd780d4dcbd
unknown <sujataekodus>
2023-02-10 10:43:12 +0530

    blog create and modify

--- resources/views/admin/master/blog/edit.blade.php
+++ resources/views/admin/master/blog/edit.blade.php
@@ -100,7 +100,24 @@
             CKEDITOR.replace( 'editor', {
                 height: 200,
                 filebrowserUploadMethod: 'form',
-                filebrowserUploadUrl: '{{ route('upload',['_token' => csrf_token() ]) }}'
+                filebrowserUploadUrl: '{{ route('upload',['_token' => csrf_token() ]) }}',
+                toolbarGroups: [
+                    { name: 'links' },
+                    { name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
+                    { name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+                    { name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+                    { name: 'forms', groups: [ 'forms' ] },
+                    { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+                    { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+                    { name: 'links', groups: [ 'links' ] },
+                    { name: 'insert', groups: [ 'insert' ] },
+                 
+                    { name: 'styles', groups: [ 'styles' ] },
+                    { name: 'colors', groups: [ 'colors' ] },
+                    { name: 'tools', groups: [ 'tools' ] },
+                    { name: 'others', groups: [ 'others' ] },
+                    { name: 'about', groups: [ 'about' ] },
+	            ]
             });
 	};
 
