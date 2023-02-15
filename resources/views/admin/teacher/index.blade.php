617510d6a6bf8f70978bc076aac2c412f42f6e10
Alok <alok.deka@ekodus.com>
2023-02-15 11:52:02 +0530

    UI bug fix

--- resources/views/admin/teacher/index.blade.php
+++ resources/views/admin/teacher/index.blade.php
@@ -72,8 +72,8 @@
                             <td>{{ $item->phone }}</td> --}}
                                     <td>{{ $item->board->exam_board ?? '' }} / Class{{ $item->assignClass->class ?? '' }} /
                                         {{ $item->assignSubject->subject_name ?? '' }}</td>
-                                    <td>{{ $item->total_experience_year ?? '0' }}Years
-                                        {{ $item->total_experience_month ?? '0' }}Months</td>
+                                    <td>{{ $item->total_experience_year ?? '0' }}-Years
+                                        {{ $item->total_experience_month ?? '0' }}-Months</td>
                                     <td>
 
                                         @if (auth()->user()->hasRole('Teacher'))
